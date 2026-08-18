0a678308e833375d548a569d16af828a2d5320a7
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-18 23:18:03 +0500

    update

---
+++ client/app/shared/components/Section-Header/ui/SectionHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex gap-4">
+        <div>
+            1
+        </div>
+        <h2>section header</h2>
+        <span>
+            under_line
+        </span>
+    </div>
+</template>
--- client/app/widgets/how-it-works/ui/HowItWorks.vue
+++ client/app/widgets/how-it-works/ui/HowItWorks.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { SectionHeader } from '~/shared/components';
+
 
 </script>
 
 <template>
     <section id="how_it_works" class="bg-default py-24">
         <UContainer>
+            <SectionHeader />
             how it works
         </UContainer>
     </section>
