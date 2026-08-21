7233b4864cd8cf47cd07672538aec14964602fb9
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-21 17:18:14 +0500

    update

--- client/app/pages/about.vue
+++ client/app/pages/about.vue
@@ -7,7 +7,8 @@ onMounted(() => {
 
 <template>
     <UContainer>
-        <div class="transition-all duration-700 ease-out delay-150" :class="revealed ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'">
+        <div class="transition-all duration-700 ease-out delay-150"
+            :class="revealed ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'">
             about page
         </div>
     </UContainer>
