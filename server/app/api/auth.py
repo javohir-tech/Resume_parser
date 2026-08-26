83f02a0977f9d6d24739e8587a8e545a58604728
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-26 23:21:51 +0500

    code review sozlandi

--- server/app/api/auth.py
+++ server/app/api/auth.py
@@ -2,10 +2,11 @@ from app.models.user import User
 from app.models.login_code import LoginCode
 from datetime import datetime, timezone
 from sqlalchemy import select, update
+from sqlalchemy.orm import selectinload
 from fastapi import APIRouter, HTTPException, Depends, status
 from app.db.session import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.core.security import create_access_token
+from app.core.security import create_access_token, create_refresh_token, verify
 
 auth_router = APIRouter()
 
@@ -13,14 +14,16 @@ auth_router = APIRouter()
 @auth_router.post("/telegram/auth/verify")
 async def vefiy_code(code: str, db: AsyncSession = Depends(get_db)):
     result = await db.execute(
-        select(LoginCode).where(LoginCode.code == code, LoginCode.is_used == False)
+        select(LoginCode)
+        .options(selectinload(LoginCode.user))
+        .filter(LoginCode.code == code, LoginCode.is_used == False)
     )
 
     login_code = result.scalar_one_or_none()
 
     if not login_code:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Natogri kode"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Natogri kod"
         )
 
     if login_code.expires_at < datetime.now(timezone.utc):
@@ -31,5 +34,25 @@ async def vefiy_code(code: str, db: AsyncSession = Depends(get_db)):
     login_code.is_used = True
     await db.commit()
 
-    token = create_access_token(login_code.user_id)
-    return {"access_token": token, "token_type": "Bearer"}
+    user = login_code.user
+
+    access_token = create_access_token(user.id)
+    refresh_token = create_refresh_token(user.id)
+
+    return {
+        "success": True,
+        "message": "successfuly logged",
+        "data": {
+            "tokens": {
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+            },
+            "user": {
+                "full_name": user.full_name,
+                "id": str(user.id),
+                "telegram_id": user.telegram_id,
+                "registered_at": user.registered_at,
+                "username": user.username,
+            },
+        },
+    }
--- server/app/core/security.py
+++ server/app/core/security.py
@@ -1,10 +1,63 @@
 import uuid
-from jose import jwt, JWTError
-from datetime import datetime, timedelta
+from jose import jwt, JWTError, ExpiredSignatureError
+from datetime import datetime, timedelta, timezone
 from app.core.config import settings
+from fastapi import Depends, status, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
+security = HTTPBearer()
 
 def create_access_token(user_id: uuid.UUID) -> str:
-    payload = {"sub": str(user_id), "exp": datetime.now() + timedelta(days=7)}
+    payload = {
+        "sub": str(user_id),
+        "exp": datetime.now(timezone.utc) + timedelta(days=1),
+        "jti": str(uuid.uuid4()),
+        "type": "access",
+    }
 
     return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
+
+
+def create_refresh_token(user_id: uuid.UUID) -> str:
+    payload = {
+        "sub": str(user_id),
+        "exp": datetime.now(timezone.utc) + timedelta(days=7),
+        "jti": str(uuid.uuid4()),
+        "type": "refresh",
+    }
+
+    return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
+
+
+def verify(token_type: str = "access",):
+    def _verify(credentials : HTTPAuthorizationCredentials = Depends(security))->str :
+        token = credentials.credentials
+        try:
+            payload = jwt.decode(
+                token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM]
+            )
+
+            user_id = payload.get("sub")
+            token_type_check = payload.get("type")
+            token_jti = payload.get("jti")
+
+            if not token_jti or not user_id:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+                )
+
+            if token_type != token_type_check:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+                )
+
+            return user_id
+        except ExpiredSignatureError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired"
+            )
+        except JWTError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid token"
+            )
+    return _verify
