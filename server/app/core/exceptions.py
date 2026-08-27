33b0f97f35f1a6d4502aa4955b2615266abf0eff
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-28 02:22:38 +0500

    update

--- server/app/core/exceptions.py
+++ server/app/core/exceptions.py
@@ -4,14 +4,17 @@ from slowapi.errors import RateLimitExceeded
 
 
 async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
-    return JSONResponse(
+    response =  JSONResponse(
         status_code=status.HTTP_429_TOO_MANY_REQUESTS,
         content={
             "success": False,
             "message": "Too many attempts. Please try again later.",
-            "retry_after": str(exc.detail),
         },
     )
+
+    retry_after = getattr(exc , "retry_after" , 60)
+    response.headers["Retry-After"] = str(retry_after)
+    return response
 
 def register_exception_handlers(app):
     app.add_exception_handler(RateLimitExceeded , rate_limit_handler)
