2b7f335344d0ae9cd4d54f4914ab2993cc23a4a1
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-09-22 18:59:20 +0500

    update

--- client/app/entities/resume/models/useGetResume.ts
+++ client/app/entities/resume/models/useGetResume.ts
@@ -6,17 +6,35 @@ export function useGetResume() {
   const loading = ref(false);
   const { showError } = useApiToast();
   const resumeStore = useResumeStore();
+  
+  let disposed =  false;
 
-  async function getResume(resume_id: string) {
+  onScopeDispose(()=>{
+    disposed = true 
+  })
+
+  async function getResume(resume_id: string):Promise<boolean> {
+
+    if(disposed) return false
+
     loading.value = true;
     try {
       const response = await getResumeFetch(resume_id);
+
+      if (disposed) return false 
       resumeStore.resume = response
+
+      return true
     //   console.log(response);
     } catch (error) {
-      showError(error);
+      if(!disposed){
+        showError(error);
+      }
+      return false
     } finally {
-      loading.value = false;
+      if(!disposed){
+        loading.value = false;
+      }
     }
   }
 
